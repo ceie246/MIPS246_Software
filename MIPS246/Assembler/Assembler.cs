2bd7bdad44d99710c7fb6ecf070507aee97cd4c9
Alfred Shi <memoriedsea@gmail.com>
2013-01-10 23:10:19 +0800

    Add MIPS246.Core and set framework to .net 4.0

---
+++ MIPS246/Assembler/Assembler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIPS246.Core.Assembler
+{
+    public class Assembler
+    {
+
+    }
+}
---
+++ MIPS246/Disassembler/Disassembler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIPS246.Core.Disassembler
+{
+    public class Disassembler
+    {
+    }
+}
