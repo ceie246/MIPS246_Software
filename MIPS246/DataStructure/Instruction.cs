0d119f175e4526719d0ae44e75a75e70f83b29da
Alfred Shi <alfred.shi@emc.com>
2013-01-11 16:48:06 +0800

    Add Fields and constructor for class instruction

--- MIPS246/DataStructure/Instruction.cs
+++ MIPS246/DataStructure/Instruction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace MIPS246.Core.DataStructure
 {
@@ -15,17 +16,63 @@ namespace MIPS246.Core.DataStructure
 
     public class Instruction
     {
+        #region Fields
         private Mnemonic mnemonic;
         private bool[] machine_code;
         private string arg1, arg2, arg3;
+        private static Hashtable Assemblertable;
+        private static Hashtable Disassembler;
+        #endregion
 
-        Instruction(Mnemonic mnemonic, bool[] machine_code, string arg1, string arg2, string arg3)
+        #region Constructors
+        static Instruction()
+        {
+            Assemblertable = new Hashtable();
+            Assemblertable.Add(Mnemonic.ADD,InitBoolArray("hh"));
+        }
+
+        Instruction(Mnemonic mnemonic, string arg1, string arg2, string arg3)
         {
             this.mnemonic = mnemonic;
-            this.machine_code = machine_code;
             this.arg1 = arg1;
             this.arg2 = arg2;
             this.arg3 = arg3;
         }
+
+        Instruction(bool[] machine_code)
+        {
+            this.machine_code = machine_code;
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public Methods
+        public bool Validate()
+        {
+            if (this.mnemonic == null)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        #endregion
+
+        #region Internal Methods
+        private static bool[] InitBoolArray(string codestring)
+        {
+            bool[] machine_code = new bool[32];
+            for (int i = 0; i < 32; i++)
+            {
+                if (codestring[i] == '0') machine_code[i] = false;
+                else machine_code[i] = true;
+            }
+            return machine_code;
+        }
+        #endregion
     }
 }
