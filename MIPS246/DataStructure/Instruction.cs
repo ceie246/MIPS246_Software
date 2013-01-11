9243b1edf435404e7d7ea3779236245c68f8fa9c
Alfred Shi <alfred.shi@emc.com>
2013-01-11 11:03:58 +0800

    ADD Mnemonic enum

--- MIPS246/DataStructure/Instruction.cs
+++ MIPS246/DataStructure/Instruction.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace MIPS246.Core.DataStructure
 {
+    enum Mnemonic
+    {
+        ADD,ADDU,SUB,SUBU,AND,OR,XOR,NOR,SLT,SLTU,SLL,SRL,SRA,SLLV,SRLV,SRAV,JR,JALR,
+        ADDI,ADDIU,ANDI,ORI,XORI,LUI,SLTI,SLTIU,LW,SW,LB,LBU,LH,LHU,SB,SH,BEQ,BNE,BGEZ,
+        BGEZAL,BGTZ,BLEZ,BLTZ,BLTZAL,J,JAL,SUBI,MOVE,NOP,LI,LA,SYSCALL
+    }
+
     public class Instruction
     {
         private Mnemonic mnemonic;
