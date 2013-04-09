7b703f4b3b8f96c77cb51439db0f4247adcccf98
cntimothy <cntimothy1990@gmail.com>
2013-04-09 21:11:03 +0800

    implement all assembler instruction

--- MIPS246/Compiler/AssemblerFac.cs
+++ MIPS246/Compiler/AssemblerFac.cs
@@ -7,216 +7,397 @@ namespace MIPS246.Core.DataStructure
 {
     public static class AssemblerFac
     {
-        public static AssemblerIns GenADD(string rd, string rs, string rt)
+        #region Private Method
+        private static AssemblerIns genrdrsrt(string operation, string rd, string rs, string rt)
         {
             AssemblerIns a = new AssemblerIns();
-            a.Op = "ADD";
+            a.Op = operation;
             a.Rd = rd;
             a.Rs = rs;
             a.Rt = rt;
             return a;
+        }
+
+        private static AssemblerIns genrdrtshamt(string operation, string rd, string rt, string shamt)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rd = rd;
+            a.Rt = rt;
+            a.Shamt = a.Shamt;
+            return a;
+        }
+
+        private static AssemblerIns genrdrtrs(string operation, string rd, string rt, string rs)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rd = rd;
+            a.Rt = rt;
+            a.Rs = rs;
+            return a;
+        }
+
+        private static AssemblerIns genrs(string operation, string rs)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rs = rs;
+            return a;
+        }
+
+        private static AssemblerIns genrtrsimm(string operation, string rt, string rs, string immediate)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rt = rt;
+            a.Rs = rs;
+            a.Immediate = immediate;
+            return a;
+        }
+
+        private static AssemblerIns genrtimm(string operation, string rt, string immediate)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rt = rt;
+            a.Immediate = immediate;
+            return a;
+        }
+
+        private static AssemblerIns genrtoffsetrs(string operation, string rt, string offset, string rs)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rt = rt;
+            a.Offset = offset;
+            a.Rs = rs;
+            return a;
+        }
+
+        private static AssemblerIns genrsrtoffset(string operation, string rs, string rt, string offset)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rs = rs;
+            a.Rt = rt;
+            a.Offset = offset;
+            return a;
+        }
+
+        private static AssemblerIns genrsoffset(string operation, string rs, string offset)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rs = rs;
+            a.Offset = offset;
+            return a;
+        }
+
+        private static AssemblerIns genaddress(string operation, string address)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Address = address;
+            return a;
+        }
+
+        private static AssemblerIns genrdrt(string operation, string rd, string rt)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rd = rd;
+            a.Rt = rt;
+            return a;
+        }
+
+        private static AssemblerIns gennull(string operation)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            return a;
+        }
+
+        private static AssemblerIns genrtlabel(string operation, string rt, string label)
+        {
+            AssemblerIns a = new AssemblerIns();
+            a.Op = operation;
+            a.Rt = rt;
+            a.Label = label;
+            return a;
+        }
+        #endregion
+
+        #region Public Method
+        public static AssemblerIns GenADD(string rd, string rs, string rt)
+        {
+            return genrdrsrt("ADD", rd, rs, rt);
+        }
+
+        public static AssemblerIns GenADDU(string rd, string rs, string rt)
+        {
+            return genrdrsrt("ADDU", rd, rs, rt);
         }
 
         public static AssemblerIns GenSUB(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "SUB";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("SUB", rd, rs, rt);
+        }
+
+        public static AssemblerIns GenSUBU(string rd, string rs, string rt)
+        {
+            return genrdrsrt("SUBU", rd, rs, rt);
         }
 
         public static AssemblerIns GenAND(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "AND";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("AND", rd, rs, rt);
         }
 
         public static AssemblerIns GenOR(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "OR";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("OR", rd, rs, rt);
         }
 
         public static AssemblerIns GenXOR(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "XOR";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("XOR", rd, rs, rt);
         }
 
         public static AssemblerIns GenNOR(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "NOR";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
-        }
-
-        public static AssemblerIns GenMathOrLog(string operation, string rd, string rs, string rt)
-        {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = operation;
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("NOR", rd, rs, rt);
         }
 
         public static AssemblerIns GenSLT(string rd, string rs, string rt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "SLT";
-            a.Rd = rd;
-            a.Rs = rs;
-            a.Rt = rt;
-            return a;
+            return genrdrsrt("SLT", rd, rs, rt);
+        }
+
+        public static AssemblerIns GenSLTU(string rd, string rs, string rt)
+        {
+            return genrdrsrt("SLTU", rd, rs, rt);
+        }
+
+        public static AssemblerIns GenSLL(string rd, string rt, string shamt)
+        {
+            return genrdrtshamt("SLL", rd, rt, shamt);
         }
 
         public static AssemblerIns GenSRL(string rd, string rt, string shamt)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "SRL";
-            a.Rd = rd;
-            a.Rt = rt;
-            a.Shamt = shamt;
-            return a;
+            return genrdrtshamt("SRL", rd, rt, shamt);
+        }
+
+        public static AssemblerIns GenSRA(string rd, string rt, string shamt)
+        {
+            return genrdrtshamt("SRA", rd, rt, shamt);
+        }
+
+        public static AssemblerIns GenSLLV(string rd, string rt, string rs)
+        {
+            return genrdrtrs("SLLV", rd, rt, rs);
+        }
+
+        public static AssemblerIns GenSRLV(string rd, string rt, string rs)
+        {
+            return genrdrtrs("SRLV", rd, rt, rs);
+        }
+
+        public static AssemblerIns GenSRAV(string rd, string rt, string rs)
+        {
+            return genrdrtrs("SRAV", rd, rt, rs);
+        }
+
+        public static AssemblerIns GenJR(string rs)
+        {
+            return genrs("JR", rs);
+        }
+
+        public static AssemblerIns GenJALR(string rs)
+        {
+            return genrs("JALR", rs);
+        }
+
+        public static AssemblerIns GenADDI(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("ADDI", rt, rs, immediate);
+        }
+
+        public static AssemblerIns GenADDIU(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("ADDIU", rt, rs, immediate);
+        }
+
+        public static AssemblerIns GenANDI(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("ANDI", rt, rs, immediate);
         }
 
         public static AssemblerIns GenORI(string rt, string rs, string immediate)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "ORI";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Immediate = immediate;
-            return a;
+            return genrtrsimm("ORI", rt, rs, immediate);
         }
 
         public static AssemblerIns GenXORI(string rt, string rs, string immediate)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "XORI";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Immediate = immediate;
-            return a;
+            return genrtrsimm("XORI", rt, rs, immediate);
         }
 
         public static AssemblerIns GenLUI(string rt, string immediate)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "LUI";
-            a.Rt = rt;
-            a.Immediate = immediate;
-            return a;
+            return genrtimm("LUI", rt, immediate);
+        }
+
+        public static AssemblerIns GenSLTI(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("SLTI", rt, rs, immediate);
+        }
+
+        public static AssemblerIns GenSLTIU(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("SLTIU", rt, rs, immediate);
         }
 
         public static AssemblerIns GenLW(string rt, string offset, string rs)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "LW";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrtoffsetrs("LW", rt, offset, rs);
         }
 
         public static AssemblerIns GenSW(string rt, string offset, string rs)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "SW";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrtoffsetrs("SW", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenLB(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("LB", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenLBU(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("LBU", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenLH(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("LH", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenLHU(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("LHU", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenSB(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("SB", rt, offset, rs);
+        }
+
+        public static AssemblerIns GenSH(string rt, string offset, string rs)
+        {
+            return genrtoffsetrs("SH", rt, offset, rs);
         }
 
         public static AssemblerIns GenBEQ(string rs, string rt, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BEQ";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsrtoffset("BEQ", rs, rt, offset);
         }
 
         public static AssemblerIns GenBNE(string rs, string rt, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BEQ";
-            a.Rt = rt;
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsrtoffset("BNE", rs, rt, offset);
         }
 
         public static AssemblerIns GenBGEZ(string rs, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BGEZ";
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsoffset("BGEZ", rs, offset);
+        }
+
+        public static AssemblerIns GenBGEZAL(string rs, string offset)
+        {
+            return genrsoffset("BGEZAL", rs, offset);
         }
 
         public static AssemblerIns GenBGTZ(string rs, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BGTZ";
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsoffset("BGTZ", rs, offset);
         }
 
         public static AssemblerIns GenBLEZ(string rs, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BLEZ";
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsoffset("BLEZ", rs, offset);
         }
 
         public static AssemblerIns GenBLTZ(string rs, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "BLTZ";
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsoffset("BLTZ", rs, offset);
         }
 
-        public static AssemblerIns GenJUMP(string operation, string rs, string offset)
+        public static AssemblerIns GenBLTZAL(string rs, string offset)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = operation;
-            a.Rs = rs;
-            a.Offset = offset;
-            return a;
+            return genrsoffset("BLTZAL", rs, offset);
         }
 
         public static AssemblerIns GenJ(string address)
         {
-            AssemblerIns a = new AssemblerIns();
-            a.Op = "J";
-            a.Address = address;
-            return a;
+            return genaddress("J", address);
         }
+
+        public static AssemblerIns GenJAL(string address)
+        {
+            return genaddress("JAL", address);
+        }
+
+        public static AssemblerIns GenSUBI(string rt, string rs, string immediate)
+        {
+            return genrtrsimm("SUBI", rt, rs, immediate);
+        }
+
+        public static AssemblerIns GenMOVE(string rd, string rt)
+        {
+            return genrdrt("MOVE", rd, rt);
+        }
+
+        public static AssemblerIns GenNOP()
+        {
+            return gennull("NOP");
+        }
+
+        public static AssemblerIns GenLI(string rt, string immediate)
+        {
+            return genrtimm("LI", rt, immediate);
+        }
+
+        public static AssemblerIns GenLA(string rt, string label)
+        {
+            return genrtlabel("LA", rt, label);
+        }
+
+        public static AssemblerIns GenSYSCALL()
+        {
+            return gennull("SYSCALL");
+        }
+
+        public static AssemblerIns GenJUMP(string operation, string rs, string offset)
+        {
+            return genrsoffset(operation, rs, offset);
+        }
+
+        public static AssemblerIns GenMathOrLog(string operation, string rd, string rs, string rt)
+        {
+            return genrdrsrt(operation, rd, rs, rt);
+        }
+
+        public static List<string> ConvertToStrList(List<AssemblerIns> cmdList)
+        {
+            List<string> cmdStrList = new List<string>();
+            foreach (AssemblerIns a in cmdList)
+            {
+                cmdStrList.Add(a.ToString());
+            }
+            return cmdStrList;
+        }
+        #endregion
     }
 }
--- MIPS246/Compiler/AssemblerIns.cs
+++ MIPS246/Compiler/AssemblerIns.cs
@@ -5,13 +5,21 @@ using System.Text;
 
 namespace MIPS246.Core.DataStructure
 {
-    internal class AssemblerIns
+    public class AssemblerIns
     {
         #region Private Fields
-        private string op = "", rs = "", rt = "", rd = "", immediate = "", offset = "", shamt = "", address = "";
+        private string op = "", rs = "", rt = "", rd = "", immediate = "", offset = "", shamt = "", address = "", label = "";
+
         #endregion
 
         #region Public Fields
+
+        public string Label
+        {
+            get { return label; }
+            set { label = value; }
+        }
+
         public string Address
         {
             get { return address; }
@@ -62,34 +70,58 @@ namespace MIPS246.Core.DataStructure
         #endregion
 
         #region Public Method
-        public string ToString()
+        new public string ToString()
         {
             StringBuilder sb = new StringBuilder();
             sb.Append(this.Op).Append(" ");
             switch (this.Op)
             {
                 case "ADD":
+                case "ADDU":
                 case "SUB":
+                case "SUBU":
                 case "AND":
                 case "OR":
                 case "XOR":
                 case "NOR":
                 case "SLT":
+                case "SLTU":
                     sb.Append(this.Rd)
                         .Append(", ")
                         .Append(this.Rs)
                         .Append(", ")
                         .Append(this.Rt);
                     break;
+                case "SLL":
                 case "SRL":
+                case "SRA":
                     sb.Append(this.Rd)
                         .Append(", ")
                         .Append(this.Rt)
                         .Append(", ")
                         .Append(this.Shamt);
                     break;
+                case "SLLV":
+                case "SRLV":
+                case "SRAV":
+                    sb.Append(this.Rd)
+                        .Append(", ")
+                        .Append(this.Rt)
+                        .Append(", ")
+                        .Append(this.Rs);
+                    break;
+                case "JR":
+                case "JALR":
+                    sb.Append(this.Rs);
+                    break;
+                case "ADDI":
+                case "ADDIU":
+                case "ANDI":
                 case "ORI":
                 case "XORI":
+                case "SLTI":
+                case "SLTIU":
+                case "SUBI":
                     sb.Append(this.Rt)
                         .Append(", ")
                         .Append(this.Rs)
@@ -97,12 +129,19 @@ namespace MIPS246.Core.DataStructure
                         .Append(this.Immediate);
                     break;
                 case "LUI":
+                case "LI":
                     sb.Append(this.Rt)
                         .Append(", ")
                         .Append(this.Immediate);
                     break;
                 case "LW":
                 case "SW":
+                case "LB":
+                case "SBU":
+                case "LH":
+                case "LHU":
+                case "SB":
+                case "SH":
                     sb.Append(this.rt)
                         .Append(", ")
                         .Append(this.Offset)
@@ -119,16 +158,33 @@ namespace MIPS246.Core.DataStructure
                         .Append(this.Offset);
                     break;
                 case "BGEZ":
+                case "BGEZAL":
                 case "BGTZ":
                 case "BLEZ":
                 case "BLTZ":
+                case "BLTZAL":
                     sb.Append(this.Rs)
                         .Append(", ")
                         .Append(this.Offset);
                     break;
                 case "J":
+                case "JAL":
                     sb.Append(this.Address);
                     break;
+                case "MOVE":
+                    sb.Append(this.Rd)
+                        .Append(", ")
+                        .Append(this.Rt);
+                    break;
+                case "NOP":
+                case "SYSCALL":
+                    break;
+                case "LA":
+                    sb.Append(this.Rt)
+                        .Append(", ")
+                        .Append(this.Label);
+                    break;
+                    
                 default:
                     //错误处理
                     break;
