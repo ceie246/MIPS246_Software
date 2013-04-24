2170f71cee748406303a3ae681c47525618cd46e
Alfred Shi <memoriedsea@gmail.com>
2013-04-24 20:43:54 +0800

    fix imm bug

---
+++ MIPS246/Compiler.GUI/CompilerMainWindow.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Assembler.GUI
+{
+    partial class CompilerMainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Assemble = new System.Windows.Forms.Button();
+            this.SourceRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SourceFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.SourcePathFileButton = new System.Windows.Forms.Button();
+            this.CompileButton = new System.Windows.Forms.Button();
+            this.SourceFileBrowseLabel = new System.Windows.Forms.Label();
+            this.OutputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.OutputFileButton = new System.Windows.Forms.Button();
+            this.OutputFileCheckBox = new System.Windows.Forms.CheckBox();
+            this.OutputPathTextBox = new System.Windows.Forms.TextBox();
+            this.BinaryRadioButton = new System.Windows.Forms.RadioButton();
+            this.HEXRadioButton = new System.Windows.Forms.RadioButton();
+            this.SourceLabel = new System.Windows.Forms.Label();
+            this.CompilerLabel = new System.Windows.Forms.Label();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.MnemonicRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // Assemble
+            // 
+            this.Assemble.Location = new System.Drawing.Point(697, 726);
+            this.Assemble.Name = "Assemble";
+            this.Assemble.Size = new System.Drawing.Size(75, 23);
+            this.Assemble.TabIndex = 0;
+            this.Assemble.Text = "Assemble";
+            this.Assemble.UseVisualStyleBackColor = true;
+            // 
+            // SourceRichTextBox
+            // 
+            this.SourceRichTextBox.Location = new System.Drawing.Point(12, 61);
+            this.SourceRichTextBox.Name = "SourceRichTextBox";
+            this.SourceRichTextBox.Size = new System.Drawing.Size(377, 430);
+            this.SourceRichTextBox.TabIndex = 1;
+            this.SourceRichTextBox.Text = "";
+            // 
+            // SourceFilePathTextBox
+            // 
+            this.SourceFilePathTextBox.Location = new System.Drawing.Point(95, 12);
+            this.SourceFilePathTextBox.Name = "SourceFilePathTextBox";
+            this.SourceFilePathTextBox.Size = new System.Drawing.Size(596, 21);
+            this.SourceFilePathTextBox.TabIndex = 2;
+            // 
+            // SourcePathFileButton
+            // 
+            this.SourcePathFileButton.Location = new System.Drawing.Point(697, 10);
+            this.SourcePathFileButton.Name = "SourcePathFileButton";
+            this.SourcePathFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SourcePathFileButton.TabIndex = 3;
+            this.SourcePathFileButton.Text = "Browse";
+            this.SourcePathFileButton.UseVisualStyleBackColor = true;
+            this.SourcePathFileButton.Click += new System.EventHandler(this.SourcePathFileButton_Click);
+            // 
+            // CompileButton
+            // 
+            this.CompileButton.ForeColor = System.Drawing.Color.Red;
+            this.CompileButton.Location = new System.Drawing.Point(697, 526);
+            this.CompileButton.Name = "CompileButton";
+            this.CompileButton.Size = new System.Drawing.Size(75, 23);
+            this.CompileButton.TabIndex = 4;
+            this.CompileButton.Text = "Compile";
+            this.CompileButton.UseVisualStyleBackColor = true;
+            this.CompileButton.Click += new System.EventHandler(this.AssembleButton_Click);
+            // 
+            // SourceFileBrowseLabel
+            // 
+            this.SourceFileBrowseLabel.AutoSize = true;
+            this.SourceFileBrowseLabel.Location = new System.Drawing.Point(12, 15);
+            this.SourceFileBrowseLabel.Name = "SourceFileBrowseLabel";
+            this.SourceFileBrowseLabel.Size = new System.Drawing.Size(77, 12);
+            this.SourceFileBrowseLabel.TabIndex = 5;
+            this.SourceFileBrowseLabel.Text = "Source File:";
+            // 
+            // OutputRichTextBox
+            // 
+            this.OutputRichTextBox.Location = new System.Drawing.Point(395, 61);
+            this.OutputRichTextBox.Name = "OutputRichTextBox";
+            this.OutputRichTextBox.ReadOnly = true;
+            this.OutputRichTextBox.Size = new System.Drawing.Size(377, 430);
+            this.OutputRichTextBox.TabIndex = 6;
+            this.OutputRichTextBox.Text = "";
+            // 
+            // OutputFileButton
+            // 
+            this.OutputFileButton.Enabled = false;
+            this.OutputFileButton.Location = new System.Drawing.Point(697, 497);
+            this.OutputFileButton.Name = "OutputFileButton";
+            this.OutputFileButton.Size = new System.Drawing.Size(75, 23);
+            this.OutputFileButton.TabIndex = 7;
+            this.OutputFileButton.Text = "Browse";
+            this.OutputFileButton.UseVisualStyleBackColor = true;
+            this.OutputFileButton.Click += new System.EventHandler(this.OutputFileButton_Click);
+            // 
+            // OutputFileCheckBox
+            // 
+            this.OutputFileCheckBox.AutoSize = true;
+            this.OutputFileCheckBox.Location = new System.Drawing.Point(14, 501);
+            this.OutputFileCheckBox.Name = "OutputFileCheckBox";
+            this.OutputFileCheckBox.Size = new System.Drawing.Size(90, 16);
+            this.OutputFileCheckBox.TabIndex = 8;
+            this.OutputFileCheckBox.Text = "Output File";
+            this.OutputFileCheckBox.UseVisualStyleBackColor = true;
+            this.OutputFileCheckBox.CheckedChanged += new System.EventHandler(this.OutputFileCheckBox_CheckedChanged);
+            // 
+            // OutputPathTextBox
+            // 
+            this.OutputPathTextBox.Enabled = false;
+            this.OutputPathTextBox.Location = new System.Drawing.Point(110, 499);
+            this.OutputPathTextBox.Name = "OutputPathTextBox";
+            this.OutputPathTextBox.Size = new System.Drawing.Size(581, 21);
+            this.OutputPathTextBox.TabIndex = 9;
+            // 
+            // BinaryRadioButton
+            // 
+            this.BinaryRadioButton.AutoSize = true;
+            this.BinaryRadioButton.Location = new System.Drawing.Point(713, 39);
+            this.BinaryRadioButton.Name = "BinaryRadioButton";
+            this.BinaryRadioButton.Size = new System.Drawing.Size(59, 16);
+            this.BinaryRadioButton.TabIndex = 10;
+            this.BinaryRadioButton.TabStop = true;
+            this.BinaryRadioButton.Text = "Binary";
+            this.BinaryRadioButton.UseVisualStyleBackColor = true;
+            this.BinaryRadioButton.CheckedChanged += new System.EventHandler(this.BinaryRadioButton_CheckedChanged);
+            // 
+            // HEXRadioButton
+            // 
+            this.HEXRadioButton.AutoSize = true;
+            this.HEXRadioButton.Checked = true;
+            this.HEXRadioButton.Location = new System.Drawing.Point(666, 39);
+            this.HEXRadioButton.Name = "HEXRadioButton";
+            this.HEXRadioButton.Size = new System.Drawing.Size(41, 16);
+            this.HEXRadioButton.TabIndex = 11;
+            this.HEXRadioButton.TabStop = true;
+            this.HEXRadioButton.Text = "HEX";
+            this.HEXRadioButton.UseVisualStyleBackColor = true;
+            this.HEXRadioButton.CheckedChanged += new System.EventHandler(this.HEXRadioButton_CheckedChanged);
+            // 
+            // SourceLabel
+            // 
+            this.SourceLabel.AutoSize = true;
+            this.SourceLabel.Location = new System.Drawing.Point(12, 41);
+            this.SourceLabel.Name = "SourceLabel";
+            this.SourceLabel.Size = new System.Drawing.Size(41, 12);
+            this.SourceLabel.TabIndex = 12;
+            this.SourceLabel.Text = "Source";
+            // 
+            // CompilerLabel
+            // 
+            this.CompilerLabel.AutoSize = true;
+            this.CompilerLabel.Location = new System.Drawing.Point(395, 40);
+            this.CompilerLabel.Name = "CompilerLabel";
+            this.CompilerLabel.Size = new System.Drawing.Size(53, 12);
+            this.CompilerLabel.TabIndex = 13;
+            this.CompilerLabel.Text = "Compiler";
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "FileDialog";
+            // 
+            // MnemonicRadioButton1
+            // 
+            this.MnemonicRadioButton1.AutoSize = true;
+            this.MnemonicRadioButton1.Location = new System.Drawing.Point(589, 38);
+            this.MnemonicRadioButton1.Name = "MnemonicRadioButton1";
+            this.MnemonicRadioButton1.Size = new System.Drawing.Size(71, 16);
+            this.MnemonicRadioButton1.TabIndex = 14;
+            this.MnemonicRadioButton1.TabStop = true;
+            this.MnemonicRadioButton1.Text = "Mnemonic";
+            this.MnemonicRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // CompilerMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.MnemonicRadioButton1);
+            this.Controls.Add(this.CompilerLabel);
+            this.Controls.Add(this.SourceLabel);
+            this.Controls.Add(this.HEXRadioButton);
+            this.Controls.Add(this.BinaryRadioButton);
+            this.Controls.Add(this.OutputPathTextBox);
+            this.Controls.Add(this.OutputFileCheckBox);
+            this.Controls.Add(this.OutputFileButton);
+            this.Controls.Add(this.OutputRichTextBox);
+            this.Controls.Add(this.SourceFileBrowseLabel);
+            this.Controls.Add(this.CompileButton);
+            this.Controls.Add(this.SourcePathFileButton);
+            this.Controls.Add(this.SourceFilePathTextBox);
+            this.Controls.Add(this.SourceRichTextBox);
+            this.Controls.Add(this.Assemble);
+            this.Name = "CompilerMainWindow";
+            this.ShowIcon = false;
+            this.Text = "MIPS246 Compiler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Assemble;
+        private System.Windows.Forms.RichTextBox SourceRichTextBox;
+        private System.Windows.Forms.TextBox SourceFilePathTextBox;
+        private System.Windows.Forms.Button SourcePathFileButton;
+        private System.Windows.Forms.Button CompileButton;
+        private System.Windows.Forms.Label SourceFileBrowseLabel;
+        private System.Windows.Forms.RichTextBox OutputRichTextBox;
+        private System.Windows.Forms.Button OutputFileButton;
+        private System.Windows.Forms.CheckBox OutputFileCheckBox;
+        private System.Windows.Forms.TextBox OutputPathTextBox;
+        private System.Windows.Forms.RadioButton BinaryRadioButton;
+        private System.Windows.Forms.RadioButton HEXRadioButton;
+        private System.Windows.Forms.Label SourceLabel;
+        private System.Windows.Forms.Label CompilerLabel;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+        private System.Windows.Forms.RadioButton MnemonicRadioButton1;
+    }
+}
+
---
+++ MIPS246/Compiler.GUI/CompilerMainWindow.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using MIPS246.Core.Assembler;
+using MIPS246.Core.DataStructure;
+
+namespace Assembler.GUI
+{
+    public partial class CompilerMainWindow : Form
+    {
+        string sourcepath = null, outputpath = null;
+        string defaultFileName = null;
+        bool isOutputFile = false;
+        bool isOutputCOE = false;
+
+        public CompilerMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputFileCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (OutputFileCheckBox.Checked == true)
+            {
+                EnableFileOutput();
+            }
+            else
+            {
+                DisableFileOutput();
+            }
+        }
+
+        private void SourcePathFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog.Filter = "Assemble file (*.asm)|*.asm";
+            OpenFileDialog.FileName = "";
+            SourceRichTextBox.Text = "";
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SourceFilePathTextBox.Text = OpenFileDialog.FileName;
+                if (File.Exists(SourceFilePathTextBox.Text))
+                {
+                    string linetext;
+                    StreamReader sr = new StreamReader(SourceFilePathTextBox.Text);
+                    while ((linetext = sr.ReadLine()) != null)
+                    {
+                        SourceRichTextBox.Text += linetext + "\r\n";
+                    }
+                    sr.Close();
+
+                    defaultFileName = Path.GetFileNameWithoutExtension(SourceFilePathTextBox.Text);
+                    
+                    sourcepath = SourceFilePathTextBox.Text;
+                    OutputRichTextBox.Text = string.Empty;
+                    CompileButton.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("File is not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                SourceFilePathTextBox.Text = string.Empty;
+            }            
+        }
+
+        private void OutputFileButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog.Filter = "Text file (*.txt)|*.txt|Coe File (*.coe)|*.coe";
+            SaveFileDialog.FileName = defaultFileName;
+            if (SaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OutputPathTextBox.Text = SaveFileDialog.FileName;
+                if (Path.GetExtension(SaveFileDialog.FileName) == ".coe")
+                {
+
+                    isOutputCOE = true;
+                }
+                else
+                {
+                    isOutputCOE = false;
+                }
+                outputpath = SaveFileDialog.FileName;
+            }
+            else
+            {
+                OutputPathTextBox.Text = string.Empty;
+            }
+        }
+
+        private void EnableFileOutput()
+        {
+            OutputFileButton.Enabled = true;
+            OutputPathTextBox.Enabled = true;
+            isOutputFile = true;
+        }
+
+        private void DisableFileOutput()
+        {
+            OutputFileButton.Enabled = false;
+            OutputPathTextBox.Enabled = false;
+            OutputPathTextBox.Text = string.Empty;
+            isOutputFile = false;
+        }
+
+        private void AssembleButton_Click(object sender, EventArgs e)
+        {
+            if (SourceFilePathTextBox.Text == string.Empty)
+            {
+                SaveFileDialog.Filter = "asm file (*.asm)|*.asm";
+                SaveFileDialog.FileName = "";
+                if (SaveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    sourcepath = SaveFileDialog.FileName;
+                    SourceFilePathTextBox.Text = sourcepath;
+                }
+            }
+
+            StreamWriter sw = new StreamWriter(sourcepath, false);
+            for (int i = 0; i < SourceRichTextBox.Lines.Length; i++)
+            {
+                sw.WriteLine(SourceRichTextBox.Lines[i]);
+            }
+            sw.Close();
+            OutputRichTextBox.Text = "";
+
+            int[] intarray=new int [1];
+            MIPS246.Core.Assembler.Assembler assembler = new MIPS246.Core.Assembler.Assembler(sourcepath, outputpath);
+            if (assembler.DoAssemble() == true)
+            {                
+                for (int i = 0; i < assembler.CodeList.Count; i++)
+                {
+                    assembler.CodeList[i].Machine_Code.CopyTo(intarray,0);
+                    if (HEXRadioButton.Checked == false)
+                    {
+                        OutputRichTextBox.Text += "0x" + String.Format("{0:X8}", assembler.CodeList[i].Address) + ":\t"  + string.Format("{0:x}", Convert.ToString(intarray[0], 2)).PadLeft(32, '0') + "\r\n";
+                    }
+                    else
+                    {
+                        OutputRichTextBox.Text += "0x" + String.Format("{0:X8}", assembler.CodeList[i].Address) + ":\t" + "0x" + string.Format("{0:x}", Convert.ToString(intarray[0], 16)).PadLeft(8, '0') + "\r\n";
+                    }
+                }
+
+                if (isOutputFile == true)
+                {
+                    assembler.Output(isOutputCOE, outputpath);
+                }                
+            }
+            else
+            {
+                OutputRichTextBox.Text = assembler.Error.ToString();
+            }
+        }
+
+        private void HEXRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(OutputRichTextBox.Text!=string.Empty)
+            {
+                AssembleButton_Click(null, null);
+            }
+            
+        }
+
+        private void BinaryRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (OutputRichTextBox.Text != string.Empty)
+            {
+                AssembleButton_Click(null, null);
+            }
+        }
+    }
+}
