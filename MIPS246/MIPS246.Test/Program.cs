c8bddef244f115698b658dd5f2986a141ae343a0
Alfred Shi <memoriedsea@gmail.com>
2013-09-02 23:24:58 +0800

    finish comparer

--- MIPS246/MIPS246.Test/Program.cs
+++ MIPS246/MIPS246.Test/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MIPS246.Core.DataStructure;
 using MIPS246.Core.TestCodeGeneator;
 using System.Configuration;
+using MIPS246.Core.ResultComparer;
 
 namespace MIPS246.Test
 {
@@ -12,19 +13,10 @@ namespace MIPS246.Test
     {
         static void Main(string[] args)
         {
-            List<Mnemonic> cmdList = new List<Mnemonic>();
-            foreach (Mnemonic m in Enum.GetValues(typeof(Mnemonic)))
-            {
-                if (ConfigurationManager.AppSettings[m.ToString()] == "true")
-                {
-                    cmdList.Add(m);
-                    Console.WriteLine(m.ToString());
-                }
-            }
+            string path1 = @"C:\Users\Alfred\Desktop\file_test.out";
+            string path2 = @"C:\Users\Alfred\Desktop\file_test2.out";
 
-           TestCodeGeneator.ConfigGeneator(100, int.Parse(ConfigurationManager.AppSettings["maximm"]), int.Parse(ConfigurationManager.AppSettings["minimm"]), cmdList);
-           TestCodeGeneator.Generate();
-
+            Console.WriteLine(ResultComparer.Compare(path1, path2));
             Console.ReadLine();
         }
     }
