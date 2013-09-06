f7fd14887298f8502c145ca5c4ec9e9ea560c17b
Alfred Shi <memoriedsea@gmail.com>
2013-09-06 17:07:20 +0800

    add MIPS246Book.aspx

---
+++ MIPS246/MIPS246.Web/MIPS246Book.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="MIPS246Book.aspx.cs" Inherits="MIPS246Book" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageBody" Runat="Server">
+    <div class="container">
+        <div id="subtitle"><h1 class="mips246font">实验指导书</h1></div>
+        <div>
+            <%=bookTable %>
+        </div>
+    </div>    
+</asp:Content>
+
---
+++ MIPS246/MIPS246.Web/MIPS246Book.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class MIPS246Book : System.Web.UI.Page
+{
+    public string bookTable;
+    private const string bookpath="C:\\MIPS246_Software\\MIPS246\\MIPS246.Web\\file\\MIPS246Book\\";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        StringBuilder sb=new StringBuilder();
+        sb.AppendLine("<table class=\"table table-hover\">");
+
+        sb.AppendLine("<tr><th width=\"50%\">文件名</th><th width=\"20%\">大小</th><th width=\"20%\">修改日期</th><th></th></tr>");
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(bookpath);
+
+        foreach (FileInfo fileinfo in directoryInfo.GetFiles())
+        {
+            sb.AppendLine("<tr><td><a href=\"./file/MIPS246Book/" + fileinfo.Name + "\">" + fileinfo.Name + "</a></td><td>" + fileinfo.Length / 1024 + " KB</td><td>" + fileinfo.LastAccessTime + "</td><td><a class=\"btn btn-primary\" href=\"./file/MIPS246Book/" + fileinfo.Name + "\">下载</td></tr>");
+        }
+
+        sb.AppendLine("</table>");
+        this.bookTable = sb.ToString();
+    }
+}
