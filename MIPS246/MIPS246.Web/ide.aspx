c635cbb39ed75bdfa19ff65348684ebf9fbb72ff
Alfred Shi <memoriedsea@gmail.com>
2013-09-07 10:54:56 +0800

    add ide.aspx

--- MIPS246/MIPS246.Web/ide.aspx
+++ MIPS246/MIPS246.Web/ide.aspx
@@ -1,13 +1,12 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="MIPS246Book.aspx.cs" Inherits="MIPS246Book" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ide.aspx.cs" Inherits="ide" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
-    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="PageBody" Runat="Server">
     <div class="container">
         <div id="subtitle"><h1 class="mips246cnfont">实验指导书</h1></div>
         <div>
-            <%=bookTable %>
+            <%=ideTable %>
         </div>
     </div>    
 </asp:Content>
---
+++ MIPS246/MIPS246.Web/ide.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ide : System.Web.UI.Page
+
+{
+    public string ideTable;
+    private const string bookpath = "C:\\MIPS246_Software\\MIPS246\\MIPS246.Web\\file\\ide\\";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("<table class=\"table table-hover\">");
+
+        sb.AppendLine("<tr><th width=\"50%\">文件名</th><th width=\"20%\">大小</th><th width=\"20%\">修改日期</th><th></th></tr>");
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(bookpath);
+
+        foreach (FileInfo fileinfo in directoryInfo.GetFiles())
+        {
+            sb.AppendLine("<tr><td><a href=\"./file/ide/" + fileinfo.Name + "\">" + fileinfo.Name + "</a></td><td>" + fileinfo.Length / 1024 + " KB</td><td>" + fileinfo.LastAccessTime + "</td><td><a class=\"btn btn-primary\" href=\"./file/ide/" + fileinfo.Name + "\">下载</td></tr>");
+        }
+
+        sb.AppendLine("</table>");
+        this.ideTable = sb.ToString();
+    }
+}
