2ac2f99c20c2cef5e6a785ef2a5459974ef39b25
Alfred Shi <memoriedsea@gmail.com>
2013-09-14 01:08:38 +0800

    add typical questions page

---
+++ MIPS246/MIPS246.Web/Questions.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Questions.aspx.cs" Inherits="question" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageBody" Runat="Server">
+    <div id="subtitle">
+       <h1 class="mips246font">常见问题</h1>
+    </div>
+    <div class="container">
+        <%=questionList %>
+    </div>
+</asp:Content>
+
---
+++ MIPS246/MIPS246.Web/Questions.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class question : System.Web.UI.Page
+{
+    public string questionList;
+    private const string questionpath = "C:\\MIPS246_Software\\MIPS246\\MIPS246.Web\\file\\Questions\\";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Session["LoginStatus"] == null)
+            {
+                Response.Redirect("Signin.aspx");
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(questionpath);
+
+        foreach (DirectoryInfo di in directoryInfo.GetDirectories())
+        {
+            sb.AppendLine("<table class=\"table table-hover\">");
+
+            sb.AppendLine("<tr><th>"+di.Name+"</th></tr>");
+            for (int i = 0; i < di.GetFiles().Count(); i++)
+            {
+                sb.AppendLine("<tr class=\"success\"><td>Q:<a href=\"./file/questions/" + di.Name + "/" + di.GetFiles()[i].Name + "\">" + di.GetFiles()[i].Name.Substring(0, di.GetFiles()[i].Name.IndexOf(".")) + "</td></tr>");
+            }
+            sb.AppendLine("</table><br />");
+        }
+        this.questionList = sb.ToString();
+    }
+}
