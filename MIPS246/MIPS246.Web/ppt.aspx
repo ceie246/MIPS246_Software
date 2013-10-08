dc5506ec7777af365a339897bab3403511d89cd6
Alfred Shi <memoriedsea@gmail.com>
2013-10-09 01:03:18 +0800

    add ppt page

---
+++ MIPS246/MIPS246.Web/ppt.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ppt.aspx.cs" Inherits="ppt" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageBody" Runat="Server">
+    <div class="container">
+        <div id="subtitle"><h1 class="mips246cnfont">PPT</h1></div>
+        <div>
+            <%=pptTable %>
+        </div>
+    </div>    
+</asp:Content>
+
---
+++ MIPS246/MIPS246.Web/ppt.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ppt : System.Web.UI.Page
+{
+    public string pptTable;
+    private const string pptpath = "C:\\MIPS246_Software\\MIPS246\\MIPS246.Web\\file\\ppT\\";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Session["LoginStatus"] == null)
+            {
+                Response.Redirect("Signin.aspx");
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("<table class=\"table table-hover\">");
+
+        sb.AppendLine("<tr><th width=\"50%\">文件名</th><th width=\"20%\">大小</th><th width=\"20%\">修改日期</th><th></th></tr>");
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(pptpath);
+
+        foreach (FileInfo fileinfo in directoryInfo.GetFiles())
+        {
+            sb.AppendLine("<tr><td><a href=\"./file/ppt/" + fileinfo.Name + "\">" + fileinfo.Name + "</a></td><td>" + fileinfo.Length / 1024 + " KB</td><td>" + fileinfo.LastAccessTime + "</td><td><a class=\"btn btn-primary\" href=\"./file/ppt/" + fileinfo.Name + "\">下载</td></tr>");
+        }
+
+        sb.AppendLine("</table>");
+        this.pptTable = sb.ToString();
+    }
+}
