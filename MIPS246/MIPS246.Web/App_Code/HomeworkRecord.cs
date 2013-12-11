293da31425aad0efa06938f68e04119604ae7711
Alfred Shi <memoriedsea@gmail.com>
2013-12-12 01:00:59 +0800

    add homework manager

---
+++ MIPS246/MIPS246.Web/App_Code/HomeworkRecord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+/// <summary>
+/// HomeworkRecord 的摘要说明
+/// </summary>
+public class HomeworkRecord
+{
+    public ObjectId _id;
+
+    public string StudentID { get; set; }
+    public string Homework1 { get; set; }
+    public string Homework2 { get; set; }
+    public string Homework3 { get; set; }
+    public string Homework4 { get; set; }
+    public string Homework5 { get; set; }
+    public string Homework6 { get; set; }
+    public string Homework7 { get; set; }
+    public string Homework8 { get; set; }
+    public string Homework9 { get; set; }
+    public string Homework10 { get; set; }
+
+    public HomeworkRecord(string studentID)
+	{
+		//
+		// TODO: 在此处添加构造函数逻辑
+		//
+
+        this.StudentID = studentID;
+        this.Homework1 = "0";
+        this.Homework2 = "0";
+        this.Homework3 = "0";
+        this.Homework4 = "0";
+        this.Homework5 = "0";
+        this.Homework6 = "0";
+        this.Homework7 = "0";
+        this.Homework8 = "0";
+        this.Homework9 = "0";
+        this.Homework10 = "0";
+        this.Homework1 = "0";
+
+	}
+}
---
+++ MIPS246/MIPS246.Web/App_Code/MIPS246HomerworkManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Text;
+
+/// <summary>
+/// MIPS246HomerworkManager 的摘要说明
+/// </summary>
+public class MIPS246HomerworkManager
+{
+    private static string connectionString;
+
+    private static string dbString;
+
+    private static string collectionString;
+
+    static MIPS246HomerworkManager()
+	{
+      connectionString = System.Configuration.ConfigurationManager.AppSettings["connectionString"];
+      dbString = System.Configuration.ConfigurationManager.AppSettings["dbString"];
+      collectionString = "homework";
+	}
+
+    public static HomeworkRecord QueryUserHomework(string id)
+    {
+        MongoServer server = MongoServer.Create(connectionString);
+        MongoDatabase db = server.GetDatabase(dbString);
+        MongoCollection collection = db.GetCollection(collectionString);
+
+        var query = new QueryDocument("StudentID", id);
+
+        return collection.FindOneAs<HomeworkRecord>(query);
+    }
+
+    public static List<HomeworkRecord> QueryAllScore()
+    {
+        MongoServer server = MongoServer.Create(connectionString);
+        MongoDatabase db = server.GetDatabase(dbString);
+        MongoCollection collection = db.GetCollection(collectionString);
+
+        List<HomeworkRecord> homeworkList = new List<HomeworkRecord>();
+        foreach (HomeworkRecord homework in collection.FindAllAs<HomeworkRecord>())
+        {
+            homeworkList.Add(homework);
+        }
+
+        return homeworkList;
+    }
+
+    public static void AddHomework(string studentID)
+    {
+        MongoServer server = MongoServer.Create(connectionString);
+        MongoDatabase db = server.GetDatabase(dbString);
+        MongoCollection collection = db.GetCollection(collectionString);
+
+        HomeworkRecord homeworkRecord = new HomeworkRecord(studentID);
+
+        collection.Insert<HomeworkRecord>(homeworkRecord);
+    }
+
+    public static void UpdateHomework(string studentID, int index)
+    {
+        MongoServer server = MongoServer.Create(connectionString);
+        MongoDatabase db = server.GetDatabase(dbString);
+        MongoCollection collection = db.GetCollection(collectionString);
+
+        var query = new QueryDocument("StudentID", studentID);
+
+        var update = new UpdateDocument { { "$set", new QueryDocument { { "Homework" + index.ToString(), "1" } } } };
+
+        collection.Update(query, update);
+    }
+
+    public static void DeleteScore(string StudentID)
+    {
+        MongoServer server = MongoServer.Create(connectionString);
+        MongoDatabase db = server.GetDatabase(dbString);
+        MongoCollection collection = db.GetCollection(collectionString);
+
+        var query = new QueryDocument("StudentID", StudentID);
+
+        collection.Remove(query);
+    }
+}
